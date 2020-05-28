a541264dd9eb6b6e243b1f9639f0767ba29dc142
bthew2003 <bthew2003@gmail.com>
2020-05-28 15:49:59 +0900

    0528_3

---
+++ web_setting/make_pyc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo python3 -m compileall src/app.py
+
+sudo cp src/__pycache__/app.cpython-37.pyc pyc/app.pyc
+#sudo chmod +x app.pyc
+
+sudo touch app.sh
+sudo chmod +x app.sh
+
+sudo echo -e "#!/bin/bash\n\nsudo python3 /opt/semtech/ampla/web_setting/pyc/app.pyc &" > app.sh
