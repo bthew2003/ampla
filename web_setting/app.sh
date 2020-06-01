506609365c23dfa560e449eb768e27eaa8fa0579
bthew2003 <bthew2003@gmail.com>
2020-06-01 09:34:09 +0900

    0601

--- web_setting/app.sh
+++ web_setting/app.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 sudo python3 /opt/semtech/ampla/web_setting/pyc/app.pyc
-=======
-sudo python3 /opt/semtech/ampla/web_setting/pyc/app.pyc &
->>>>>>> 56c5bd7a32ba86a49fccdae0bc842e5477264c92
--- web_setting/make_pyc.sh
+++ web_setting/make_pyc.sh
@@ -8,8 +8,4 @@ sudo cp src/__pycache__/app.cpython-37.pyc pyc/app.pyc
 sudo touch app.sh
 sudo chmod +x app.sh
 
-<<<<<<< HEAD
 sudo echo -e "#!/bin/bash\n\nsudo python3 /opt/semtech/ampla/web_setting/pyc/app.pyc" > app.sh
-=======
-sudo echo -e "#!/bin/bash\n\nsudo python3 /opt/semtech/ampla/web_setting/pyc/app.pyc &" > app.sh
->>>>>>> 56c5bd7a32ba86a49fccdae0bc842e5477264c92
