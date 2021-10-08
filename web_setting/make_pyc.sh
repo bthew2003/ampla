4a4876ee9c1b61441250a1034e96df594c7bbaa3
bthew2003 <bthew2003@gmail.com>
2021-10-08 13:32:24 +0900

    0.9.5

---
+++ web_setting/make_pyc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo python3 -m compileall src/*.py
+
+sudo cp src/__pycache__/app.cpython-37.pyc pyc/app.pyc
+sudo cp src/__pycache__/I2C_EEPROM_Class.cpython-37.pyc pyc/I2C_EEPROM_Class.pyc
+sudo cp src/__pycache__/bTest_Class.cpython-37.pyc pyc/bTest_Class.pyc
+sudo cp src/__pycache__/snCheck.cpython-37.pyc pyc/snCheck.pyc
+
+sudo touch app.sh
+sudo chmod +x app.sh
+
+#sudo echo -e "#!/bin/bash\n\nsudo python3 /opt/semtech/ampla/web_setting/pyc/app.pyc" > app.sh
